e3a3b030afde3f27f25075bb08fe610b9801d8bb
Wamiq Reyaz <wamiq.para@kaust.edu.sa>
2020-10-27 12:50:44 +0300

    Updated gitignore

---
+++ sweep_walls_lifull.sh
@@ -0,0 +1,19 @@
+#!/bin/bash --login
+#SBATCH -N 1
+#SBATCH --array=0-40
+#SBATCH --export=ALL,NCCL_SOCKET_IFNAME=eth0
+#SBATCH --partition=batch
+#SBATCH -J walls
+#SBATCH --cores-per-socket=5
+#SBATCH -o slurm/%A_%a.out
+#SBATCH -e slurm/%A_%a.err
+#SBATCH --time=06:00:00
+#SBATCH --mem=24G
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:v100
+#SBATCH -A conf-gpu-2020.11.23
+
+conda activate faclab
+
+wandb agent wamreyaz/Walls/5wg0l8sm
