664e8dd77d404514c8e6730ae5f1254540bfb755
Wamiq Reyaz <wamiq.para@kaust.edu.sa>
2020-10-31 09:53:03 +0300

    Filter warnings for tensorboard. Change to integer optimization

--- optimize_triples.py
+++ optimize_triples.py
@@ -12,29 +12,40 @@ def load_pickle(fname):
         return pickle.load(fd)
 
 if __name__ == '__main__':
-    ROOT_DIR = '/home/parawr/Projects/floorplan/samples/v2_tuned_triples_0.9/'
+
+    import h5py
+
+    save_file = h5py.File('triples_0.5_0.5.hdf5', 'w')
+    group = save_file.create_group('optimized')
+
+
+    ROOT_DIR = '/home/parawr/Projects/floorplan/samples/triples_0.5/'
     verts = natsorted(glob(os.path.join(ROOT_DIR, '*.npz')))
 
-    OTHER_DIR = './samples/v2_tuned_triples_0.9'
-    SAVE_DIR = os.path.join(OTHER_DIR, 'nodes_0.9_0.9')
+    OTHER_DIR = '/mnt/ibex/Projects/floorplan/samples/triples_0.5'
+    SAVE_DIR = os.path.join(OTHER_DIR, 'nodes_0.5_0.5')
 
-    if not os.path.exists(SAVE_DIR):
-        os.makedirs(SAVE_DIR, exist_ok=True)
+    # if not os.path.exists(SAVE_DIR):
+    #     os.makedirs(SAVE_DIR, exist_ok=True)
 
     from node import Node, Floor, LPSolver
     from random import random as rand
     from tqdm import tqdm
 
+    from collections import OrderedDict
+    save_dict = OrderedDict()
+    count = 0
     for name in tqdm(verts):
 
         curr_file = name #.replace('temp_0.9', 'temp_1.0')
         base_name = os.path.basename(curr_file)
         root_name = os.path.splitext(base_name)[0]
 
-        horiz_file = os.path.join(OTHER_DIR, 'edges', 'h_0.9', root_name + '.pkl')
-        vert_file = os.path.join(OTHER_DIR, 'edges', 'v_0.9', root_name + '.pkl')
+        horiz_file = os.path.join(OTHER_DIR, 'edges', 'h', root_name + '.pkl')
+        vert_file = os.path.join(OTHER_DIR, 'edges', 'v', root_name + '.pkl')
 
-        save_file = os.path.join(SAVE_DIR, root_name + '.npy')
+        save_file = os.path.join(SAVE_DIR, root_name + '.npz')
+        # file_name = r
 
         try:
             horiz_edges = load_pickle(horiz_file)
@@ -56,8 +67,8 @@ if __name__ == '__main__':
             w = vertices[idx, 1]
             h = vertices[idx, 2]
 
-            widths.append(w/64.0)
-            heights.append(h/64.0)
+            widths.append(w)
+            heights.append(h)
             floor.add_room(Node.from_data(id, rand(), rand(), rand(), rand()))
 
         floor.add_horiz_constraints(horiz_edges)
@@ -68,12 +79,27 @@ if __name__ == '__main__':
         solver._add_xloc_constraints(widths, eps=0)
         solver._add_yloc_constraints(heights, eps=0)
         solver.same_line_constraints()
+        solver.maximal_boxes_constraint(True)
 
         try:
             solver.solve(mode=None)
             solver._set_floor_data()
         except:
             continue
+
+        save_dict[root_name] = solver.get_floor().get_room_array() #.ravel()
+        count += 1
+
+        if count > 100:
+            break
+
+
+    # print(save_dict)
+    # for k, v in save_dict.items():
+    #     group.create_dataset(k, data=v)
+
+
+        #
 
         with open(save_file, 'wb') as fd:
             np.save(fd, solver.get_floor().get_room_array())
